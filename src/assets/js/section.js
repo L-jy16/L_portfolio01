fe86b6b3d07ab37ab01525f9ad59971a2ebcbe8d
jeong202 <144635774+L-jy16@users.noreply.github.com>
2023-12-14 12:09:23 +0900

    2023.12.14

--- src/assets/js/section.js
+++ src/assets/js/section.js
@@ -126,95 +126,36 @@ export function section() {
     //     markers: false
     // })
 
-    // // section3
-    // let currentIndex = 0;
-    // const marqueeWrap = document.querySelector(".marquee")
-    // const marquee_heading_Clone = marqueeWrap.firstElementChild.cloneNode(true);
-    // marqueeWrap.appendChild(marquee_heading_Clone);
+    // section3
 
-    // const marquee_heading = document.querySelectorAll(".marquee .marquee-heading");
+    // desc 숨긴거 오버시 보여주기
+    let desc = document.querySelectorAll(".work_site");
 
-    // setInterval(() => {
-    //     currentIndex++;
+    desc.forEach(descitem => {
+        descitem.addEventListener("mouseover", function () {
+            this.querySelector(".desc").style.display = "block"
+        })
+        descitem.addEventListener("mouseout", function () {
+            this.querySelector(".desc").style.display = "none"
+        })
+    })
 
-    //     gsap.to(marquee_heading, {
-    //         duration: 3,
-    //         x: -200,
-    //         scrollTrigger: {
-    //             trigger: marqueeWrap,
-    //             start: "top 50%",
-    //             end: "bottom 20%",
-    //             scrub: true
-    //         }
-    //     });
+    // 가로형 모드
+    // gsap.registerPlugin(ScrollTrigger);
+    // const horSection = gsap.utils.toArray(".work_site");
 
-    //     if (currentIndex == 6) {
-    //         setTimeout(() => {
-
-    //             gsap.to(marquee_heading, {
-    //                 duration: 0,
-    //                 x: 0,
-    //                 scrollTrigger: {
-    //                     trigger: marqueeWrap,
-    //                     start: "top 50%",
-    //                     end: "bottom 20%",
-    //                     scrub: true
-    //                 }
-    //             });
-    //             currentIndex = 0;
-    //         }, 700)
-    //     }
-    // }, 3000)
-
-    // // section3 이미지 youtube
-    // gsap.to("#youtube", {
-    //     yPercent: 20,
+    // gsap.to(horSection, {
+    //     xPercent: -120 * (horSection.length - 1),
     //     ease: "none",
-    //     duration: 0.5,
     //     scrollTrigger: {
-    //         trigger: ".work_info.i1",
-    //         start: "top bottom",
-    //         end: "bottom top",
-    //         markers: false,
-    //         scrub: true
-    //     }
-    // });
-    // gsap.to("#myuduck", {
-    //     yPercent: 20,
-    //     ease: "none",
-    //     duration: 0.5,
-    //     scrollTrigger: {
-    //         trigger: ".work_info.i2",
-    //         start: "top bottom",
-    //         end: "bottom top",
-    //         markers: false,
-    //         scrub: true
-    //     }
-    // });
-
-    // gsap.to("#movie", {
-    //     yPercent: 20,
-    //     ease: "none",
-    //     duration: 0.5,
-    //     scrollTrigger: {
-    //         trigger: ".work_info.i3",
-    //         start: "top bottom",
-    //         end: "bottom top",
-    //         markers: false,
-    //         scrub: true
-    //     }
-    // });
-
-    // gsap.to("#blog", {
-    //     yPercent: 20,
-    //     ease: "none",
-    //     duration: 0.5,
-    //     scrollTrigger: {
-    //         trigger: ".work_info.i4",
-    //         start: "top bottom",
-    //         end: "bottom top",
-    //         markers: false,
-    //         scrub: true
+    //         trigger: "#section3",
+    //         start: "top top",
+    //         end: "+=3000",
+    //         pin: true,
+    //         scrub: 1,
+    //         // markers: true,
+    //         invalidateOnRefresh: true,
+    //         anticipatePin: 1,
     //     }
     // });
 
