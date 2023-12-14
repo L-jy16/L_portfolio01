e6a16f37252cb656728a328f98b3654dd1ce8994
jeong202 <144635774+L-jy16@users.noreply.github.com>
2023-12-14 14:13:41 +0900

    2023.12.14

--- src/assets/js/section.js
+++ src/assets/js/section.js
@@ -140,24 +140,24 @@ export function section() {
     //     })
     // })
 
-    // // 가로형 모드
-    // gsap.registerPlugin(ScrollTrigger);
-    // const horSection = gsap.utils.toArray(".work_site");
+    // 가로형 모드
+    gsap.registerPlugin(ScrollTrigger);
+    const horSection = gsap.utils.toArray(".work_site");
 
-    // gsap.to(horSection, {
-    //     xPercent: -120 * (horSection.length - 1),
-    //     ease: "none",
-    //     scrollTrigger: {
-    //         trigger: "#section3",
-    //         start: "top top",
-    //         end: "+=3000",
-    //         pin: true,
-    //         scrub: 1,
-    //         // markers: true,
-    //         invalidateOnRefresh: true,
-    //         anticipatePin: 1,
-    //     }
-    // });
+    gsap.to(horSection, {
+        xPercent: -120 * (horSection.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#section3",
+            start: "top top",
+            end: "+=3000",
+            pin: true,
+            scrub: 1,
+            // markers: true,
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+        }
+    });
 
     // section4
     let s0;
