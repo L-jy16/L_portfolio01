cad789cf29063819b198d59b0df91ed3e3ebb1db
jeong202 <144635774+L-jy16@users.noreply.github.com>
2023-12-15 18:04:10 +0900

    2023.12.15

--- src/assets/js/section.js
+++ src/assets/js/section.js
@@ -65,11 +65,47 @@ export function section() {
 
     // // section2
 
+    let i1;
+
+    gsap.set(".aboutMe__En.split.i1 .word", {
+        opacity: 0.3,
+    })
+
+    gsap.set(".skill__detail.img", {
+        opacity: 0,
+    })
+
+    gsap.set(".infoBox.i2, .infoBox.i3, .infoBox.i4, .infoBox.i5, .infoBox.i6, .infoBox.i7", {
+        opacity: 0.6,
+        yPercent: 200
+    });
+
+    setTimeout(() => {
+        i1 = gsap.timeline({ paused: true });
+
+        i1.to(".aboutMe__En.split.i1 .word", { opacity: 1, ease: "power1.out", duration: 0.4, stagger: 0.1, })
+        i1.to(".skill__detail.img", { opacity: 1, ease: "power1.out", duration: 0.4, stagger: 0.1, })
+        i1.to(".infoBox.i2", { opacity: 1, yPercent: 0, ease: "power4.out", duration: 0.8, stagger: 0.0351, }, "-=0.5")
+        i1.to(".infoBox.i3", { opacity: 1, yPercent: 0, ease: "power4.out", duration: 0.8, stagger: 0.0351, }, "-=0.5")
+        i1.to(".infoBox.i4", { opacity: 1, yPercent: 0, ease: "power4.out", duration: 0.8, stagger: 0.0351, }, "-=0.5")
+        i1.to(".infoBox.i5", { opacity: 1, yPercent: 0, ease: "power4.out", duration: 0.8, stagger: 0.0351, }, "-=0.5")
+        i1.to(".infoBox.i6", { opacity: 1, yPercent: 0, ease: "power4.out", duration: 0.8, stagger: 0.0351, }, "-=0.5")
+        i1.to(".infoBox.i7", { opacity: 1, yPercent: 0, ease: "power4.out", duration: 0.8, stagger: 0.0351, }, "-=0.5")
+    }, 1000)
+
+
+    ScrollTrigger.create({
+        trigger: "#section2",
+        start: "top center",
+        end: "+=100%",
+        onEnter: () => i1.play(),
+        onLeaveBack: () => i1.reverse(),
+        markers: false
+    })
+
     // let a1;
 
-    // gsap.set(".aboutme_En.split.a1 .line, .skill_info.split.a3 .line", {
-    //     opacity: 0,
-    // })
+
 
     // // gsap.set(".aboutme_ko.split.a2, .info_box.split.a4, .info_box.split.a5, .info_box.split.a6,.info_box.split.a7", {
     // //     opacity: 0.6,
